5e8467327c02e6b33419518f7b31e17848868c33
Wally Ho <whohoo@qq.com>
2014-01-28 18:35:46 +0800

    直接读取content.txt文件, 把需要嵌入的文字放在此文档内

--- run.py
+++ run.py
@@ -2,12 +2,13 @@
 
 from TTFRender import generate, get_token_list
 
-
-str = """
-一
-"""
-
+# read content.txt file
+txtFile = open('content.txt')
+str = txtFile.read()
+print(str)
 token_list = get_token_list(str)
-ttf_file   = "new.ttf"
+src_ttf_file = "新蒂小丸子小学版"
+out_ttf_file   = "xindixiaowanzhi.ttf"
 
-generate(token_list, "kai", ttf_file)
+# generate new font file
+generate(token_list, src_ttf_file, out_ttf_file)
